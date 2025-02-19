f6ea43e07ea5c4236ecb2250a317dbe0f8b57ec1
bowman <fancycode@gmail.com>
2025-02-19 13:46:33 -0800

    fix $   xxx   $ not display correct in Obsidian

--- macclip.py
+++ macclip.py
@@ -11,7 +11,9 @@ def convert_math_syntax(input_text):
     output_text = re.sub(r'\\\[(.*?)\\\]', r'$$\1$$', input_text, flags=re.DOTALL)
     # turn  \(...\) into $...$ format
     output_text = re.sub(r'\\\((.*?)\\\)', r'$\1$', output_text, flags=re.DOTALL)
-
+    # turn $ xxx $ into $xxx$ format
+    output_text = re.sub(r'\$\s*(.*?)\s*\$', r'$\1$', output_text, flags=re.DOTALL)
+    
     return output_text
 
 
